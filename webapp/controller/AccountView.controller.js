caaea98a2ce746ed146f8036cb1411475e5faa65
Hemanth Yamsani <hemanth.yamsani@colruytgroup.com>
2025-03-26 16:51:16 +0000

    updated show data logic - parse only 2 values

--- webapp/controller/AccountView.controller.js
+++ webapp/controller/AccountView.controller.js
@@ -12,9 +12,6 @@ sap.ui.define([
     return Controller.extend("y4cr2r020e249.controller.AccountView", {
 
         onInit: function () {
-            // var oSimpleForm = this.byId("accountSimpleForm");
-            // oSimpleForm.addStyleClass("noTitles");
-            // Create an empty data object for the model
             var oDocDatePicker = this.byId("inputDocumentDate");
             var oPostDatePicker = this.byId("inputPostingDate");
             var oPostPeriodPicker = this.byId("inputPostingPeriod");
@@ -130,105 +127,20 @@ sap.ui.define([
             });
         },
 
-        // onCompanyCodeValueHelpRequest: function (oEvent) {
-        //     var that = this;
-        //     var oModel = this.getView().getModel("oModel");
-        //     var oTableModel = this.getView().getModel("oTableModel");
-        //     sap.ui.core.BusyIndicator.show(0);
-        //     var oParams = {
-        //         $select: "CustomerID,CompanyName"
-        //     };
-        //     oModel.read("/Customers", {
-        //         urlParameters: oParams,
-        //         success: function (oData, response) {
-        //             sap.ui.core.BusyIndicator.hide();
-        //             oTableModel.setProperty("/aCompanyCode", oData.results);
-        //             // TableIndex: oEvent.getSource().getBindingContext("aCompanyCode").sPath.split("/")[2]
-        //             if (!that.odlgCompanyCode) {
-        //                 that.odlgCompanyCode = sap.ui.xmlfragment("y4cr2r020e249.fragment.CompanyCodeDialog", that);
-        //                 that.getView().addDependent(that.odlgCompanyCode, that);
-
-        //                 var oSearchField = sap.ui.getCore().byId(that.odlgCompanyCode.getId() + "-searchField");
-                
-        //                 // Attach the liveChange event
-        //                 if (oSearchField) {
-        //                     oSearchField.attachLiveChange(that.handleLiveSearch, that);
-        //                 }
-
-        //             }
-        //             that.odlgCompanyCode.open();
-        //         },
-        //         error: function (oError) {
-        //             console.error("Error reading data:", oError);
-        //         }
-        //     });
-
-        // },
-
-        // handleSearch: function (oEvent) {
-		// 	var sValue = oEvent.getParameter("value");
-		// 	var oFilter = new Filter("CustomerID", FilterOperator.Contains, sValue);
-		// 	var oBinding = oEvent.getSource().getBinding("items");
-		// 	oBinding.filter([oFilter]);
-		// },
-
-        // handleLiveSearch: function (oEvent) {
-        //     var sValue = oEvent.getParameter("newValue");
-        //     var oBinding = this.odlgCompanyCode.getBinding("items");
-            
-        //     if (!sValue || sValue === "") {
-        //         // Clear all filters if search field is empty
-        //         oBinding.filter([]);
-        //     } else {
-        //         // Create filters for both CustomerID and CompanyName
-        //         var oFilterCustomerID = new Filter("CustomerID", FilterOperator.Contains, sValue);
-        //         var oFilterCompanyName = new Filter("CompanyName", FilterOperator.Contains, sValue);
-                
-        //         // Combine the filters with OR operator
-        //         var oCombinedFilter = new Filter({
-        //             filters: [oFilterCustomerID, oFilterCompanyName],
-        //             and: false  // This means OR operation
-        //         });
-                
-        //         // Apply the filter to the binding
-        //         oBinding.filter(oCombinedFilter);
-        //     }
-        // },
-
-        // handleSearch: function (oEvent) {
-        //     var sValue = oEvent.getParameter("value");
-        //     var oBinding = oEvent.getSource().getBinding("items");
-            
-        //     if (!sValue || sValue === "") {
-        //         oBinding.filter([]);
-        //     } else {
-        //         var oFilterCustomerID = new Filter("CustomerID", FilterOperator.Contains, sValue);
-        //         var oFilterCompanyName = new Filter("CompanyName", FilterOperator.Contains, sValue);
-        //         var oCombinedFilter = new Filter({
-        //             filters: [oFilterCustomerID, oFilterCompanyName],
-        //             and: false
-        //         });
-        //         oBinding.filter(oCombinedFilter);
-        //     }
-        // },
 
         onCompanyCodeValueHelpRequest: function (oEvent) {
             var that = this;
-            // Only load data if it hasn't been loaded yet
             var oTableModel = this.getView().getModel("oTableModel");
             var aCompanyData = oTableModel.getProperty("/aCompanyCode");
             
-            // Create dialog if it doesn't exist
             if (!this._oCompCodeDialog) {
                 this._oCompCodeDialog = sap.ui.xmlfragment("y4cr2r020e249.fragment.CompanyCodeDialog", this);
                 this.getView().addDependent(this._oCompCodeDialog);
                 
-                // Attach event handlers just once when creating the dialog
                 this._oCompCodeDialog.attachSearch(this.onSearchFilter, this);
                 this._oCompCodeDialog.attachLiveChange(this.onSearchFilter, this);
             }
             
-            // Only load data if we don't have it already
             if (!aCompanyData || aCompanyData.length === 0) {
                 sap.ui.core.BusyIndicator.show(0);
                 var oModel = this.getView().getModel("oModel");
@@ -246,26 +158,21 @@ sap.ui.define([
                     error: function (oError) {
                         sap.ui.core.BusyIndicator.hide();
                         console.error("Error reading data:", oError);
-                        // Show error message to user
                         sap.m.MessageToast.show("Error loading company data");
                     }
                 });
             } else {
-                // If we already have data, just open the dialog
                 this._oCompCodeDialog.open();
             }
         },
         
-        // Single unified search handler for both live change and search events
         onSearchFilter: function (oEvent) {
-            // Get search value, checking both possible parameter names
             var sValue = oEvent.getParameter("value") || oEvent.getParameter("newValue") || "";
             var oBinding = oEvent.getSource().getBinding("items");
             
             if (!sValue) {
                 oBinding.filter([]);
             } else {
-                // Create filters for both CustomerID and CompanyName
                 var oFilters = [
                     new Filter("CustomerID", FilterOperator.Contains, sValue),
                     new Filter("CompanyName", FilterOperator.Contains, sValue)
@@ -282,35 +189,24 @@ sap.ui.define([
             var that = this;
             var oSelectedItem = oEvent.getParameter("selectedItem");
             
-            // If item was selected (not just canceled)
             if (oSelectedItem) {
                 var sCustomerID = oSelectedItem.getCells()[0].getText();
-                
-                // Set the selected CustomerID to your input field
                 this.byId("inputCompanyCode").setValue(sCustomerID);
-                
-                // If you want to trigger the change event programmatically
-                // this.onCompanyCodeChange();
             }
         },
 
         onCurrencyValueHelpRequest: function(oEvent) {
-            // If you don't have an OData service for currencies, you can define a local model
             var aCurrencies = [
                 { code: "USD", name: "US Dollar" },
                 { code: "EUR", name: "Euro" },
                 { code: "GBP", name: "British Pound" },
                 { code: "JPY", name: "Japanese Yen" }
-                // Add more currencies as needed
             ];
             
-            // Create a model for the currencies
             var oCurrencyModel = new sap.ui.model.json.JSONModel({
                 currencies: aCurrencies
             });
             
-            // Create a dialog or fragment to show the currencies
-            // This is a simplified example - you might want to use a fragment
             if (!this._oDialog) {
                 this._oDialog = new sap.m.SelectDialog({
                     title: "Select Currency",
@@ -352,17 +248,14 @@ sap.ui.define([
             var oTableModel = this.getView().getModel("oTableModel");
             var aTerritoriesData = oTableModel.getProperty("/aTerritories");
             
-            // Create dialog if it doesn't exist
             if (!this._oHeaderTextDialog) {
                 this._oHeaderTextDialog = sap.ui.xmlfragment("y4cr2r020e249.fragment.HeaderTextDialog", this);
                 this.getView().addDependent(this._oHeaderTextDialog);
                 
-                // Attach event handlers just once when creating the dialog
                 this._oHeaderTextDialog.attachSearch(this.onHeaderTextSearchFilter, this);
                 this._oHeaderTextDialog.attachLiveChange(this.onHeaderTextSearchFilter, this);
             }
             
-            // Only load data if we don't have it already
             if (!aTerritoriesData || aTerritoriesData.length === 0) {
                 sap.ui.core.BusyIndicator.show(0);
                 var oModel = this.getView().getModel("oModel");
@@ -383,13 +276,11 @@ sap.ui.define([
                     }
                 });
             } else {
-                // If we already have data, just open the dialog
                 this._oHeaderTextDialog.open();
             }
 
         },
         onHeaderTextSearchFilter: function (oEvent) {
-            // Get search value, checking both possible parameter names
             var sValue = oEvent.getParameter("value") || oEvent.getParameter("newValue") || "";
             var oBinding = oEvent.getSource().getBinding("items");
             
@@ -439,7 +330,6 @@ sap.ui.define([
 
             }
             else {
-                // Important: add this else branch to open the dialog when data exists
                 that.pRefDialog.open();
             }
             
@@ -454,92 +344,6 @@ sap.ui.define([
             };
         },
 
-        // onShowData: function() {
-        //     var that = this;
-        //     var oModel = this.getView().getModel("oModel");
-        //     var oTableModel = this.getView().getModel("oTableModel");
-        //     sap.ui.core.BusyIndicator.show(0);
-        //     oModel.read("/Customers", {
-        //         success: function(oData, response) {
-        //             sap.ui.core.BusyIndicator.hide();
-        //             oTableModel.setProperty("/aTableResults", oData.results);
-        //             if(!that.odlgCustomerData) {
-        //                 that.odlgCustomerData = sap.ui.xmlfragment("y4cr2r020e249.fragment.ShowTableData", that);
-        //                 that.getView().addDependent(that.odlgCustomerData, that);
-
-        //             }
-        //             that.odlgCustomerData.open();
-        //         },
-        //         error: function(oError) {
-        //             console.error("Error reading data:", oError);
-        //         }
-        //     });
-        // },
-
-        // onCustomersPress: function(oEvent) {
-        //     var that = this;
-        //     var oModel = this.getView().getModel("oModel");
-        //     var oTableModel = this.getView().getModel("oTableModel");
-        //     oModel.read("/Customers", {
-        //         success: function(oData, response) {
-        //             sap.ui.core.BusyIndicator.hide();
-        //             oTableModel.setProperty("/aTableResults", oData.results);
-        //         }
-        //     },
-        // },
-
-        // onShowData: function () {
-        //     var that = this;
-        //     sap.ui.core.BusyIndicator.show(0);
-        //     if (!that.odlgCustomerData) {
-        //         that.odlgCustomerData = sap.ui.xmlfragment("y4cr2r020e249.fragment.ShowTableData", that);
-        //         that.getView().addDependent(that.odlgCustomerData);
-        //     }
-        //     sap.ui.core.BusyIndicator.hide();
-        //     that.odlgCustomerData.open();
-        // },
-
-        // onPress: function(sPath, sID) {
-        //     var that = this;
-        //     var oModel = this.getView().getModel("oModel");
-        //     var oTableModel = this.getView().getModel("oTableModel");
-
-        //     sap.ui.core.BusyIndicator.show(0);
-
-        //     oModel.read(sPath, {
-        //         success: function(oData) {
-        //             sap.ui.core.BusyIndicator.hide();
-        //             oTableModel.setProperty("/" + sID, oData.results);
-
-        //             // // Set all tables to invisible
-        //             // var aTables = ["tblCustomers", "tblEmployees", "tblSuppliers"];
-        //             // aTables.forEach(function(tableId) {
-        //             //     that.byId(tableId).setVisible(false);
-        //             // });
-
-        //             that.byId("sID").setVisible(true);
-
-        //         },
-        //         error: function(oError) {
-        //             sap.ui.core.BusyIndicator.hide();
-        //             console.error("Error reading data:", oError);
-        //         }
-        //     });
-        // },
-
-
-
-        // onCustomersPress: function() {
-        //     this.onPress("/Customers", "tblCustomers");
-        // },
-
-        // onEmployeesPress: function() {
-        //     this.onPress("/Employees", "tblEmployees");
-        // },
-
-        // onSuppliersPress: function() {
-        //     this.onPress("/Suppliers", "tblSuppliers");
-        // },
         onShowData: function() {
             var that = this;
             sap.ui.core.BusyIndicator.show(0);
@@ -549,14 +353,12 @@ sap.ui.define([
                 that.getView().addDependent(that.odlgCustomerData);
             }
             
-            // Reset the active table when opening the dialog
             var oModel = this.getView().getModel();
             oModel.setProperty("/tableKey", "");
             
             sap.ui.core.BusyIndicator.hide();
             that.odlgCustomerData.open();
         },
-        // Modify the onPress function to update tableKey
         onPress: function (sPath, tableKey) {  // Add tableKey parameter
             var that = this;
             var oModel = this.getView().getModel("oModel");
@@ -578,22 +380,24 @@ sap.ui.define([
                 }
             });
         },
-        // Update button press handlers to pass tableKey
+
         onCustomersPress: function (oEvent) {
             var that = this;
             var oButton = oEvent.getSource();
             var oView = this.getView();
             that.customerID = oButton.getId();
             // id = oView.getId();
-            this.onPress("/Customers", "c");  // pass "c" as tableKey
+            this.onPress("/Customers", "c");
         },
 
         onEmployeesPress: function () {
-            this.onPress("/Employees", "Employees", "e");  // pass "e" as tableKey 
+            // this.onPress("/Employees", "Employees", "e");
+            this.onPress("/Employees", "e");
         },
 
         onSuppliersPress: function () {
-            this.onPress("/Suppliers", "Suppliers", "s");  // pass "s" as tableKey
+            // this.onPress("/Suppliers", "Suppliers", "s");
+            this.onPress("/Suppliers", "s");
         },
 
         onCloseDialog: function() {
@@ -630,9 +434,7 @@ sap.ui.define([
         onPostingPeriodChange: function (oEvent) {
             this.validateDate(oEvent);
         },
-        // Regular expression for DD.MM.YYYY format
-        // var dateRegex = /^(0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[0-2])\.\d{4}$/;
-
+        
         onFiscalYearChange: function (oEvent) {
             var sValue = oEvent.getParameter("value");
             var oInput = oEvent.getSource();
@@ -678,41 +480,30 @@ sap.ui.define([
         },
 
         onDelete: function () {
-            // Get the table control by its ID.
             var oTable = this.byId("accountTable");
 
-            // Retrieve the array of selected indices.
             var aSelectedIndices = oTable.getSelectedIndices();
             if (aSelectedIndices.length === 0) {
                 sap.m.MessageToast.show("Please select at least one row to delete.");
                 return;
             }
 
-            // Get the model and the current items array.
             var oModel = this.getView().getModel();
             var aItems = oModel.getProperty("/items");
 
-            // Sort the indices in descending order to avoid re-indexing issues.
             aSelectedIndices.sort(function (a, b) { return b - a; });
 
-            // Loop over the sorted indices and remove the corresponding items.
             aSelectedIndices.forEach(function (iIndex) {
                 aItems.splice(iIndex, 1);
             });
 
-            // Update the model with the new items array.
             oModel.setProperty("/items", aItems);
-
-            // Clear the table's selection to update the UI.
             oTable.clearSelection();
 
             sap.m.MessageToast.show("Selected row(s) deleted.");
         },
 
-
-        // Event handler for notification button press
         onNotificationPress: function (oEvent) {
-            // Get the notifications array from the model
             var oModel = this.getView().getModel();
             var aNotifications = oModel.getProperty("/notifications");
 
